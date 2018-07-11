e840fa935de94ea1811cc894f246cbcfe2e80a80
unknown <jordiamir@gmail.com>
2018-07-11 19:35:13 +0300

    shopping cart works! (need to add 3 more items)

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,50 +1,19 @@
-/*Variables that represent shopping items*/ 
-
-var inequation = {
-    item: `<p><strong><span style="font-size: 17px;">אי שוויון עם ערך מוחלט</span></strong></p>
-            <ul>
-                <li>חזרה ותזכורת</li>
-                <li>אי שוויון עם ערך מוחלט</li>
-                <li>אי שוויון עם רב איבר בערך מוחלט</li>
-            </ul>
-            <button class="btn btn-link removeBtn">הסר</button>`,
-    price: 12,
-    set: false
-}
-
-var wordProblems = {
-    item: `<p><strong><span style="font-size: 17px;">בעיות מילוליות</span></strong></p>
-            <ul>
-                <li>רקע</li>
-                <li>תרגול נעלמים ושליפת נתונים</li>
-                <li>הצבת משוואות ופתרון בעיות מילוליות</li>
-            </ul>
-            <button class="btn btn-link removeBtn">הסר</button>`,
-    price: 12,
-    set: false
-}
-
-
-var itemTableData =`<tr>
-                        <th class="text-center" scope="row">1</th>
-                        <td class="item"></td>
-                        <td class="text-center itemPrice"></td> 
-                    </tr>`
+/*Variables:*/ 
 
 var totalPrice = 0;
-
-
+var chapterName = localStorage.getItem('chapterName');
+var cartNotEmpty = localStorage.getItem('cartNotEmpty');
+var y = localStorage.getItem('numOfItems');
 
 /*--------------------------------------------------------------------------------------------------------------*/
 
 
-
-$('.items').hide();
-
-
-
-$('.shoppingCartLink').click(function(e){
-
+/*Clicking 'add to cart' cause the following:
+    1. Finding the specific chapter that was clicked.
+    2. Marking the shopping cart as 'not empty' (so that the orange square with the number of items will be shown).
+    3. Counting the number of items (later use in orange square and in numbering the items at the shopping cart table).
+*/
+$('.shoppingCartLink').click(function(){
     localStorage.setItem('chapterName', $('.shoppingCartLink').attr('id'));
 
     localStorage.setItem('cartNotEmpty', true);
@@ -52,60 +21,131 @@ $('.shoppingCartLink').click(function(e){
     var x = localStorage.getItem('numOfItems');     
     x++;
     localStorage.setItem('numOfItems', x);
-
 });
 
 
-var chapterName = localStorage.getItem('chapterName');
-var cartNotEmpty = localStorage.getItem('cartNotEmpty');
-var y = localStorage.getItem('numOfItems');
+//Number of items on the little orange square:
+$('.itemsNum').html(y);
 
 
-$('.'+chapterName).attr("disabled", 'disabled');
 
-
-if (cartNotEmpty){
-
+$('.items').hide();
+//Show number of items next to cart:
+if (cartNotEmpty){  
     $('.items').show();
-    $('.itemRows').prepend(itemTableData);
+}
 
-    if (chapterName === 'inequation'){
-        localStorage.setItem('inequation',(chapterName == 'inequation'));
-    }
-    if (localStorage.getItem('inequation')){
-        $('.item').prepend(inequation.item);
-        $('.itemPrice').prepend(inequation.price + '  ש"ח');
-        totalPrice = totalPrice + inequation.price;
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/
+//reducing code returns:
+var chapters = ['inequation', 'wordProblems'];
+if (chapterName === 'inequation'){
+    localStorage.setItem(chapterName, true);
+    localStorage.setItem(chapterName+'Price', 12);
+    localStorage.setItem(chapterName+'Item', `<tr class="inequationRow">
+                                                <th class="text-center" scope="row"></th>
+                                                <td class="item">
+                                                    <p><strong><span style="font-size: 17px;">אי שוויון עם ערך מוחלט</span></strong></p>
+                                                    <ul>
+                                                        <li>חזרה ותזכורת</li>
+                                                        <li>אי שוויון עם ערך מוחלט</li>
+                                                        <li>אי שוויון עם רב איבר בערך מוחלט</li>
+                                                    </ul>
+                                                    <button class="btn btn-link removeBtn" id="inequation">הסר</button>
+                                                </td>
+                                                <td class="text-center inequationPrice"></td>               
+                                            </tr>`);
+}
+
+if (chapterName === 'wordProblems'){
+    localStorage.setItem(chapterName, true);
+    localStorage.setItem(chapterName+'Price', 12);
+    localStorage.setItem(chapterName+'Item', `<tr class="wordProblemsRow">
+                                                <th class="text-center" scope="row"></th>
+                                                <td class="item">
+                                                    <p><strong><span style="font-size: 17px;">בעיות מילוליות</span></strong></p>
+                                                    <ul>
+                                                        <li>רקע</li>
+                                                        <li>תרגול נעלמים ושליפת נתונים</li>
+                                                        <li>הצבת משוואות ופתרון בעיות מילוליות</li>
+                                                    </ul>
+                                                    <button class="btn btn-link removeBtn" id="wordProblems">הסר</button>
+                                                </td>
+                                                <td class="text-center wordProblemsPrice"></td>               
+                                            </tr>`);
+}
+
+
+
+
+for (i=0; i<=chapters.length; i++){
+
+    if (chapterName === chapters[i]){
+        localStorage.setItem(chapters[i], true);
     }
 
-    if (chapterName === 'wordProblems'){
-        localStorage.setItem('wordProblems',(chapterName == 'wordProblems'));
-    }
-    if (localStorage.getItem('wordProblems')){
-        $('.item').prepend(wordProblems.item);
-        $('.itemPrice').prepend(wordProblems.price + '  ש"ח');
-        totalPrice = totalPrice + wordProblems.price;
+    if (localStorage.getItem(chapters[i])){
+        var price = Number(localStorage.getItem(chapters[i]+'Price'));
+        var item = localStorage.getItem(chapters[i]+'Item');
+        $('.'+chapters[i]).attr("disabled", 'disabled');
+        $('.itemRows').prepend(item);
+        $('.'+chapters[i]+'Price').prepend(price + '  ש"ח');
+        totalPrice = totalPrice + price;
+    
+    
     }
 
 }
 
 
-$('.itemTotal').append(totalPrice );
 
-$('.itemsNum').html(y);
+$('.removeBtn').click(function(){
+
+    var chapter = this.id;
+    var price = Number(localStorage.getItem(chapter+'Price'));
+
+    totalPrice = totalPrice - price;
+    $('.itemTotal').html(totalPrice);
+    console.log("totalPrice = " + totalPrice);
+   
+
+    $('.'+chapter+'Row').hide();
+
+    localStorage.removeItem('chapterName');
+    localStorage.removeItem(chapter);
+    y--;
+    localStorage.setItem('numOfItems', y);
+    $('.itemsNum').html(y);    
+    
+});
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//numbering the shopping cart items:
+for (i=1; i<=y; i++){
+    $('tbody tr:nth-child('+i+') th').append(i);
+}
+
+
+//insert cart total price:
+$('.itemTotal').append(totalPrice);
 
 
 
 
-/*-----------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*testings:*/
 
 console.log("Empty Cart = " + !cartNotEmpty);
 console.log("items = " + y);
-console.log("chapter = " + chapterName);
-console.log("inequation = " + inequation.set);
-console.log("wordProblems = " + wordProblems.set);
+console.log("chapterName = " + chapterName);
+
 
 
 // localStorage.removeItem('cartNotEmpty');
@@ -113,3 +153,10 @@ console.log("wordProblems = " + wordProblems.set);
 // localStorage.removeItem('chapterName');
 // localStorage.removeItem('wordProblems');
 // localStorage.removeItem('inequation');
+// localStorage.removeItem('wordProblemsPrice');
+// localStorage.removeItem('inequationPrice');
+// localStorage.removeItem('wordProblemsItem');
+// localStorage.removeItem('inequationItem');
+
+
+
