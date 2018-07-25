232ce59e797953f625cfc11029c5b7d5cbf4c3d7
unknown <jordiamir@gmail.com>
2018-07-25 18:33:37 +0300

    php address test2

--- public/php/contact.php
+++ public/php/contact.php
@@ -1,17 +1,19 @@
 <?php
-    $name = $_POST['name'];
-    $visitorEmail = $_POST['email'];
-    $message = $_POST['message'];
+    if ($_POST['submit']){
+        $name = $_POST['name'];
+        $visitorEmail = $_POST['email'];
+        $message = $_POST['message'];
 
-    $emailFrom = 'haizler.co.il';
-    $emailSubject = "New Form Submission";
-    $emailBody = "User Name: $name.\n".
-                    "User Email: $visitorEmail.\n".
-                    "User Message: $message.\n";
+        $emailFrom = 'haizler.co.il';
+        $emailSubject = "New Form Submission";
+        $emailBody = "User Name: $name.\n".
+                        "User Email: $visitorEmail.\n".
+                        "User Message: $message.\n";
 
-    $to = "jordiamir@gmail.com";
-    $headers = "From: $emailFrom \r\n";
-    $headers .= "Replay-To: $visitorEmail \r\n";
-    mail($to,$emailSubject,$emailBody,$headers);
-    header("Location: contact.html");
+        $to = "jordiamir@gmail.com";
+        $headers = "From: $emailFrom \r\n";
+        $headers .= "Replay-To: $visitorEmail \r\n";
+        mail($to,$emailSubject,$emailBody,$headers);
+        header("Location: contact.html");
+    }
 ?>
