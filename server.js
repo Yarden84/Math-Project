109e931ada2b2a1db3395520c0ec4bc51625d0de
unknown <jordiamir@gmail.com>
2018-07-29 20:01:03 +0300

    server.js modified

--- server.js
+++ server.js
@@ -37,44 +37,44 @@ app.use(bodyParser.json());
 
 
 
-// app.post('/send', (req, res) => {
-//     const output =
-//     `<p>You have a new contact request</p>
-//     <h3>Contact Details</h3>
-//     <ul>
-//         <li>Name: ${req.body.name}</li>
-//         <li>email: ${req.body.email}</li>
-//         <li>Message: ${req.body.message}</li>
-//     </ul>
-//     `;
+app.post('/send', (req, res) => {
+    const output =
+    `<p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>email: ${req.body.email}</li>
+        <li>Message: ${req.body.message}</li>
+    </ul>
+    `;
 
-//     let transporter = nodemailer.createTransport({
-//         host: 'smtp.gmail.com',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: 'haizlermath@gmail.com', // generated ethereal user
-//             pass: 'learnmath' // generated ethereal password
-//         }
-//     });
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'haizlermath@gmail.com', // generated ethereal user
+            pass: 'learnmath' // generated ethereal password
+        }
+    });
 
-//     // setup email data with unicode symbols
-//     let mailOptions = {
-//         from: '"Yourself" <haizlermath@gmail.com>', // sender address
-//         to: 'haizlermath@gmail.com', // list of receivers
-//         subject: 'Contact Message', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: output // html body
-//     };
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Yourself" <haizlermath@gmail.com>', // sender address
+        to: 'haizlermath@gmail.com', // list of receivers
+        subject: 'Contact Message', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
 
-//     // send mail with defined transport object
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         console.log('Message sent: %s', info.messageId);
-//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-//         res.render('contact', {msg:"Message has been sent"});
-//     });
-// });
+        res.render('contact', {msg:"Message has been sent"});
+    });
+});
